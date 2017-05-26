bcb4d33d8e33309fa3022975cd89cb1c312f72ca
George Powell <george.powell@thehutgroup.com>
2017-05-26 11:52:26 +0100

    reverting port changes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function(request, response) {
-  response.send("hello");
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send('hello');
 });
 
-app.listen(80, function() {
-  console.log('Node app is running on port', 80);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 
