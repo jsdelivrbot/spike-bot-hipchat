67fa9867bb6d692b1cd75f7e1eaabd2f0c57073b
George Powell <george.powell@thehutgroup.com>
2017-05-26 14:24:16 +0100

    integrates with hipchat

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var request = require('request');
 
 app.set('port', (process.env.PORT || 8080));
 
 var router = express.Router();
 
 router.use(function(req, res, next) {
-	console.log('Something is happening.');
+	console.log('Request:');
 	next();
 });
 
-router.get('/', function(req, res) {
-	res.json({ message: 'working!' });	
-});
-
-router.route('/:value')
+router.route('/hipchat/post/:host/:room/:authtoken')
 	.put(function(req, res) {
-		res.json({ message: 'echo:' + req.params.value });	
+		var host = req.params.host;
+		var room = req.params.room;
+		var authtoken = req.params.authtoken;
+		var hipchatNotificationUrl =
+		"https://" + host + 
+		".hipchat.com/v2/room/" + room +
+		"/notification?auth_token=" + authtoken;
+
+		var httpBody = '{"message_format": "text", "message":"What\'s that spike?" }'
+
+		request(
+			{
+				method: 'POST',
+				uri: hipchatNotificationUrl,
+				headers: { "Content-Type": "application/json" },
+				body: httpBody
+			},
+			function (error, response, body) {
+			    console.log('response: '+ response.statusCode)
+			    console.log(body)
+			}
+		)
+
+		res.json({ message: 'success' });
 	});
 
 app.use('/api', router);
 
-app.listen(app.get('port'));
+app.listen(app.get('port'), function() {
+	console.log('Running on port:' + app.get('port'));
+});
+
+
